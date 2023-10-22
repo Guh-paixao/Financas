ac90762ca4ecd44cd70791c42cd9c047d9705031
guh-paixao <gustavopaixao.dev@gmail.com>
2023-10-22 02:29:26 -0300

    Estilizando a area de transferência vazia

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://localhost:3333'
-    baseURL: 'http://100.70.124.6:3333'
+    baseURL: 'http://100.70.124.4:3333'
 })
 
 export default api;
